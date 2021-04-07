620886cdbc08eb90f86f3a3e43fb0feff67d8c06
robertobirijnr <robertobirijnr@hotmail.com>
2021-04-07 12:10:15 +0000

    navigation and hero section

---
+++ components/TheNavBar.vue
@@ -0,0 +1,85 @@
+<template>
+    <nav>
+     <nuxt-link to="/">
+      <span> Bob.</span>
+     </nuxt-link>
+
+     <ul class="menu">
+       <li><nuxt-link to="/">Home</nuxt-link></li>
+       <li><nuxt-link to="/">Skills</nuxt-link></li>
+       <li><nuxt-link to="/">Recent Projects</nuxt-link></li>
+       <li><nuxt-link to="/">Contact</nuxt-link></li>
+     </ul>
+
+     <nuxt-link class="hey" to="/">
+       Say Hi
+     </nuxt-link>
+   </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+ul{
+  list-style: none;
+}
+
+a{
+  text-decoration: none;
+}
+
+nav{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-shadow: 5px 10px 30px rbga(0,0,0,0.02);
+  position: fixed;
+  left: 0;
+  top:0;
+  width: 100%;
+  z-index: 1; 
+  background-color: #fff;
+}
+
+nav ul{
+  display: flex;
+}
+
+nav ul li a{
+  height: 40px;
+  line-height: 43px;
+  margin: 3px;
+  padding: 0px 22px;
+  display: flex;
+  font-size: 0.8rem;
+  text-transform: uppercase;
+  font-weight: 500;
+  color: #585b5b;
+  letter-spacing: 1px;
+}
+
+.hey{
+  color: #39bfbd;
+  font-weight: 500;
+  font-size: 0.9rem;
+  border-bottom:2px solid #40c6c4
+}
+
+nav ul li a:hover{
+  background-color: #2adabc;
+  color: #fff;
+  box-shadow: 5px 10px 30px rgba(64,198,196,0.3);
+  transition: all ease 0.2s;
+}
+
+span{
+    font-size: 40px;
+    font-weight: 700;
+    color: #2adabc;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,49 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'robertobiri',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    title: 'robertobirijnr',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico '
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,700;1,100;1,200;1,300&display=swap'
+      }
+
     ]
   },
 
+
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -46,6 +64,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
